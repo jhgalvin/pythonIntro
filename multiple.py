4350df99f5da2ec984220dc8297bd9cbf1782dc0
Jonah Galvin <60234353+jhgalvin@users.noreply.github.com>
2020-08-04 17:21:03 -0500

    Add files via upload

---
+++ multiple.py
@@ -0,0 +1,15 @@
+#Check if a number is a multiple of 3 and a multiple of 7
+#use modules and conditionals to do this
+
+x = 21
+y = 16
+
+if x % 3 == 0 and x % 7 == 0:
+  print("x is divisible by 3 and 7")
+else:
+  print("x is not divisible by 3 and/or 7")
+
+if y % 3 == 0 and y % 7 == 0:
+  print("y is divisible by 3 and 7")
+else:
+  print("y is not divisible by 3 and/or 7")
