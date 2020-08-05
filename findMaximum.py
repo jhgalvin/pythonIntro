7f69a49aae04305f711566bc981e0d89f9fc3861
Jonah Galvin <60234353+jhgalvin@users.noreply.github.com>
2020-08-05 14:02:55 -0500

    Add files via upload

--- findMaximum.py
+++ findMaximum.py
@@ -2,9 +2,9 @@
 
 def maximum(number1, number2):
     if(number1 > number2):
-        print("The first number entered, {}, is greater!".format(number1))
+        return number1
     elif(number1 < number2):
-        print("The second number entered, {}, is greater!".format(number2))
+        return number2
     else:
         print("The numbers are equal!")
 
@@ -14,4 +14,5 @@ number1 = int(input())
 print("Enter the second number: ")
 number2 = int(input())
 
-maximum(number1, number2)
+max = maximum(number1, number2)
+print("The greater number is {}!".format(max))
